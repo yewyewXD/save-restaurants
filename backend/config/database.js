a8c56f48fd80b9c5480b5010b24a12efc42f2d18
yewyewXD <yewyew6933@gmail.com>
2021-07-04 11:40:28 +0200

    complete database config

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,8 +4,9 @@ const connectDB = async () => {
   try {
     const mongoDb = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
-      useCreateIndex: true,
       useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
     });
 
     console.log(`MongoDB Connected: ${mongoDb.connection.host}`);
