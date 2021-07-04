59acf40860ad12a713bdb8b607cbd5a255aa18d0
yewyewXD <yewyew6933@gmail.com>
2021-07-04 11:40:45 +0200

    setup localhost and connect server database

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,19 @@
+const express = require("express");
+const connectDB = require("./config/database");
 const dotenv = require("dotenv");
 
 // middleware
 dotenv.config();
+connectDB();
+const app = express();
+
+// port
+const PORT = process.env.PORT || 5000;
+app.listen(
+  PORT,
+  console.log(
+    `Server is running in ${
+      process.env.NODE_ENV || "development"
+    } on port ${PORT}`
+  )
+);
