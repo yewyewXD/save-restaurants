cb76a58189ef67ec8b7b0665b7c088e0844bb0ae
yewyewXD <yewyew6933@gmail.com>
2021-07-05 17:03:39 +0200

    setup children and metaTitle props

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { FC } from "react";
 
-const Layout = () => {
+interface Props {
+  metaTitle: string;
+}
+
+const Layout: FC<Props> = ({ children, metaTitle }) => {
   return (
     <div>
+      <div>my meta title is {metaTitle}</div>
       <div>header</div>
-
+      <div>{children}</div>
       <div>footer</div>
     </div>
   );
