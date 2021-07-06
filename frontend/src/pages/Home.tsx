1920139bb9baa0afcd591d6e66f989f926d8f6c1
yewyewXD <yewyew6933@gmail.com>
2021-07-06 09:13:03 +0200

    setup basic login form

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,9 +2,25 @@ import React, { FC } from "react";
 import Layout from "../components/Layout";
 
 const Home: FC = () => {
+  function handleLogin() {
+    console.log("going to login");
+  }
+
   return (
     <Layout metaTitle={"MY TITLE"}>
-      <div>body of home</div>
+      <div>
+        <div>
+          <label htmlFor="loginEmail">Enter Email</label>
+          <input type="email" id="loginEmail" />
+        </div>
+
+        <div>
+          <label htmlFor="loginPassword">Enter Password</label>
+          <input type="password" id="loginPassword" />
+        </div>
+
+        <button onClick={handleLogin}>Login</button>
+      </div>
     </Layout>
   );
 };
