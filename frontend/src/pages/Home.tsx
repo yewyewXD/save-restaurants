a345e89062500eb9a94981a268a8a7729d07609e
yewyewXD <yewyew6933@gmail.com>
2021-07-06 09:39:28 +0200

    handle update form states

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from "react";
+import React, { FC, ChangeEvent, useState } from "react";
 import Layout from "../components/Layout";
 
 interface RegisterInfo {
@@ -18,9 +18,15 @@ const Home: FC = () => {
     console.log("going to login");
   }
 
-  //   function handleUpdateForm(e){
-  // if(e.persist)e.persist()
-  //   }
+  function handleUpdateForm(e: ChangeEvent<HTMLInputElement>) {
+    e.persist();
+    setRegisterInfo((prevInfo) => {
+      return {
+        ...prevInfo,
+        [e.target.id]: e.target.value,
+      };
+    });
+  }
 
   return (
     <Layout metaTitle={"MY TITLE"}>
@@ -31,17 +37,28 @@ const Home: FC = () => {
             value={registerInfo.displayName}
             type="text"
             id="displayName"
+            onChange={handleUpdateForm}
           />
         </div>
 
         <div>
           <label htmlFor="email">Enter Email</label>
-          <input value={registerInfo.email} type="email" id="email" />
+          <input
+            value={registerInfo.email}
+            onChange={handleUpdateForm}
+            type="email"
+            id="email"
+          />
         </div>
 
         <div>
           <label htmlFor="password">Enter Password</label>
-          <input value={registerInfo.password} type="password" id="password" />
+          <input
+            value={registerInfo.password}
+            onChange={handleUpdateForm}
+            type="password"
+            id="password"
+          />
         </div>
 
         <button onClick={handleLogin}>Login</button>
