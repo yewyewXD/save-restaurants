f262ebe6118f4c359400f4337144277f8ccfb96e
yewyewXD <yewyew6933@gmail.com>
2021-07-06 08:16:24 +0200

    setup home page with layout

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from "react";
+import Layout from "../components/Layout";
+
+const Home: FC = () => {
+  return (
+    <Layout metaTitle={"MY TITLE"}>
+      <div>body of home</div>
+    </Layout>
+  );
+};
+
+export default Home;
