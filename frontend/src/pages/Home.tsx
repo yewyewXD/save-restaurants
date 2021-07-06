662bb8f1fd79884ae08b77ea834616006ceda88f
yewyewXD <yewyew6933@gmail.com>
2021-07-06 09:25:51 +0200

    setup states for register fields

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,22 +1,47 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import Layout from "../components/Layout";
 
+interface RegisterInfo {
+  displayName: string;
+  email: string;
+  password: string;
+}
+
 const Home: FC = () => {
+  const [registerInfo, setRegisterInfo] = useState<RegisterInfo>({
+    displayName: "",
+    email: "",
+    password: "",
+  });
+
   function handleLogin() {
     console.log("going to login");
   }
+
+  //   function handleUpdateForm(e){
+  // if(e.persist)e.persist()
+  //   }
 
   return (
     <Layout metaTitle={"MY TITLE"}>
       <div>
         <div>
-          <label htmlFor="loginEmail">Enter Email</label>
-          <input type="email" id="loginEmail" />
+          <label htmlFor="displayName">Enter Username</label>
+          <input
+            value={registerInfo.displayName}
+            type="text"
+            id="displayName"
+          />
         </div>
 
         <div>
-          <label htmlFor="loginPassword">Enter Password</label>
-          <input type="password" id="loginPassword" />
+          <label htmlFor="email">Enter Email</label>
+          <input value={registerInfo.email} type="email" id="email" />
+        </div>
+
+        <div>
+          <label htmlFor="password">Enter Password</label>
+          <input value={registerInfo.password} type="password" id="password" />
         </div>
 
         <button onClick={handleLogin}>Login</button>
