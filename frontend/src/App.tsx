95aaab8b64a659d246040a8d4f1c03e033d12cc9
yewyewXD <yewyew6933@gmail.com>
2021-08-05 11:31:36 +0200

    add ModalProvider to app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,18 @@
 import React, { Suspense, lazy } from "react";
+import { ModalProvider } from "./context/modal/ModalState";
 const { BrowserRouter, Switch, Route } = require("react-router-dom");
 const Home = lazy(() => import("./pages/Home"));
 
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Suspense fallback="Loading...">
-          <Route path="/" component={Home} exact />
-        </Suspense>
-      </Switch>
+      <ModalProvider>
+        <Switch>
+          <Suspense fallback="Loading...">
+            <Route path="/" component={Home} exact />
+          </Suspense>
+        </Switch>
+      </ModalProvider>
     </BrowserRouter>
   );
 }
