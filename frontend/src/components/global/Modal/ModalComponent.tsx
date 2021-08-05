4f5dc323c282e7f590ed31aabdab4d053cdc64fb
yewyewXD <yewyew6933@gmail.com>
2021-08-05 11:58:23 +0200

    create separate ModalComponent

---
+++ frontend/src/components/global/Modal/ModalComponent.tsx
@@ -0,0 +1,37 @@
+import React, { useRef, useEffect, FC, cloneElement } from "react";
+const Modal = require("react-modal");
+const { useLocation } = require("react-router-dom");
+
+interface Props {
+  isShowing: boolean;
+  content: any;
+  close: () => {};
+}
+
+const ModalComponent: FC<Props> = ({ isShowing, content, close }) => {
+  const location = useLocation();
+  const locationRef = useRef(location.pathname);
+
+  useEffect(() => {
+    if (location.pathname !== locationRef.current) {
+      locationRef.current = location.pathname;
+      close();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [location.pathname]);
+
+  return (
+    <Modal
+      appElement={document.getElementById("root")}
+      isOpen={isShowing}
+      className="Modal Modal--autoWidth"
+      overlayClassName="ModalOverlay"
+      closeTimeoutMS={250}
+      onRequestClose={close}
+      shouldCloseOnOverlayClick={true}
+    >
+      {content ? cloneElement(content, { close }) : null}
+    </Modal>
+  );
+};
+export default ModalComponent;
