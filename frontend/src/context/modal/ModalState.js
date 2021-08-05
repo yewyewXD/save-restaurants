cd80f7992eb0d7e9217e66a80c9686973fc1b971
yewyewXD <yewyew6933@gmail.com>
2021-08-05 11:58:13 +0200

    move ModalComponent out

--- frontend/src/context/modal/ModalState.js
+++ frontend/src/context/modal/ModalState.js
@@ -1,42 +1,6 @@
-import React, {
-  createContext,
-  useState,
-  useCallback,
-  useContext,
-  useEffect,
-  useRef,
-  cloneElement,
-} from "react";
-import Modal from "react-modal";
-import { useLocation } from "react-router-dom";
+import React, { createContext, useState, useCallback, useContext } from "react";
+import ModalComponent from "../../components/global/Modal/ModalComponent";
 import { debounce } from "../../utils/modal.utils";
-
-function ModalComponent({ isShowing = true, content, close }) {
-  const location = useLocation();
-  const locationRef = useRef(location.pathname);
-
-  useEffect(() => {
-    if (location.pathname !== locationRef.current) {
-      locationRef.current = location.pathname;
-      close();
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [location.pathname]);
-
-  return (
-    <Modal
-      appElement={document.getElementById("root")}
-      isOpen={isShowing}
-      className="Modal Modal--autoWidth"
-      overlayClassName="ModalOverlay"
-      closeTimeoutMS={250}
-      onRequestClose={close}
-      shouldCloseOnOverlayClick={true}
-    >
-      {content ? cloneElement(content, { close }) : null}
-    </Modal>
-  );
-}
 
 export const ModalContext = createContext({});
 
