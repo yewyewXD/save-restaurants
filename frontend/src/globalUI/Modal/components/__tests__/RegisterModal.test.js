93ca956f616fe7364759063a8756819bfa9b6d69
yewyewXD <yewyew6933@gmail.com>
2021-08-06 22:40:53 +0200

    clean up after each test

--- frontend/src/globalUI/Modal/components/__tests__/RegisterModal.test.js
+++ frontend/src/globalUI/Modal/components/__tests__/RegisterModal.test.js
@@ -1,6 +1,10 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, cleanup } from "@testing-library/react";
 import HeroSection from "../../../../pages/Home/components/HeroSection";
 // import RegisterModal from '../RegisterModal';
+
+afterEach(() => {
+  cleanup();
+});
 
 test("should render register button on hero section", () => {
   render(<HeroSection />);
