1dbbc49c60399a797de47da93c693a475d1b0655
yewyewXD <yewyew6933@gmail.com>
2021-08-06 09:07:45 +0200

    handle show RegisterModal

--- frontend/src/pages/Home/components/HeroSection.tsx
+++ frontend/src/pages/Home/components/HeroSection.tsx
@@ -1,15 +1,33 @@
 import React from "react";
+import { useModal } from "../../../context/modal/ModalState";
+import RegisterModal from "../../../globalUI/Modal/components/RegisterModal";
 
 const HeroSection = () => {
+  const { handleShowModal } = useModal();
+
+  function showLoginModal() {
+    handleShowModal(<div>test login</div>);
+  }
+
+  function showRegisterModal() {
+    handleShowModal(<RegisterModal />);
+  }
+
   return (
     <section className="flex w-full h-screen justify-center items-center">
       <div>
         <h1>welcome to this website</h1>
         <div className="flex">
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
+          <button
+            onClick={showLoginModal}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"
+          >
             Login
           </button>
-          <button className="ml-2 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
+          <button
+            onClick={showRegisterModal}
+            className="ml-2 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"
+          >
             Register
           </button>
         </div>
