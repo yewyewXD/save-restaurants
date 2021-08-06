3f9141b189c51d50fe62137b040e804903026089
yewyewXD <yewyew6933@gmail.com>
2021-08-06 12:13:43 +0200

    add notification provider to app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,21 @@
 import React, { Suspense, lazy } from "react";
 import { ModalProvider } from "./context/modal/ModalState";
+import { NotificationProvider } from "./context/notification/NotificationState";
 const { BrowserRouter, Switch, Route } = require("react-router-dom");
 const Home = lazy(() => import("./pages/Home/Home"));
 
 function App() {
   return (
     <BrowserRouter>
-      <ModalProvider>
-        <Switch>
-          <Suspense fallback="Loading...">
-            <Route path="/" component={Home} exact />
-          </Suspense>
-        </Switch>
-      </ModalProvider>
+      <NotificationProvider>
+        <ModalProvider>
+          <Switch>
+            <Suspense fallback="Loading...">
+              <Route path="/" component={Home} exact />
+            </Suspense>
+          </Switch>
+        </ModalProvider>
+      </NotificationProvider>
     </BrowserRouter>
   );
 }
