8727ae6d66e8a3e7930910db78b778698d0696bc
yewyewXD <yewyew6933@gmail.com>
2021-08-07 19:46:46 +0200

    show logout and name if has authName

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,9 +1,29 @@
 import React, { FC } from "react";
+import { useAuth } from "../../context/auth/AuthState";
 import HeroSection from "./components/HeroSection";
 
 const Home: FC = () => {
+  const { authName, clearUserAuth } = useAuth();
   return (
     <main>
+      <nav className="h-12 ">
+        <div className="my-2 h-full w-full flex justify-between mx-5">
+          <div>
+            {authName ? `Welcome back, ${authName}!` : "You are not logged in"}
+          </div>
+
+          {authName && (
+            <button
+              onClick={clearUserAuth}
+              data-testid="register-btn"
+              className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+            >
+              Logout
+            </button>
+          )}
+        </div>
+      </nav>
+
       <HeroSection />
     </main>
   );
