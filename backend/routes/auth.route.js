989d3c5cb28e56fd80377a932060c97f607cdab2
yewyewXD <yewyew6933@gmail.com>
2021-08-08 16:44:53 +0200

    add auth middleware to login

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -5,10 +5,11 @@ const {
   loginUser,
   googleLoginUser,
 } = require("../controllers/auth.controller");
+const auth = require("../middleware/auth.middleware");
 
 // Register user
 router.post("/register", registerUser);
-router.post("/login", loginUser);
+router.post("/login", auth, loginUser);
 router.post("/google-login", googleLoginUser);
 
 module.exports = router;
