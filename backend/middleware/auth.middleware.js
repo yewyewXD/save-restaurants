057beb4fb6b0942657f452fd38b96d0ddc725ebe
yewyewXD <yewyew6933@gmail.com>
2021-08-08 08:21:46 +0200

    remove check google auth token in middleware

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -3,17 +3,12 @@ import jwt from "jsonwebtoken";
 const auth = async (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
-    const isJwtAuth = token.length < 500;
 
-    let decodedData;
-
-    if (token && isJwtAuth) {
-      decodedData = jwt.verify(token, process.env.JWT_SECRET);
+    if (token) {
+      const decodedData = jwt.verify(token, process.env.JWT_SECRET);
       req.userId = decodedData?.id;
-    } else {
-      decodedData = jwt.decode(token);
-      req.userId = decodedData?.sub;
     }
+
     next();
   } catch (err) {
     console.log(err);
