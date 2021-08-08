22cc15e3804cc0c031f0178e3e98392e7565ae08
yewyewXD <yewyew6933@gmail.com>
2021-08-08 07:26:35 +0200

    add google login route and controller

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser, loginUser } = require("../controllers/auth.controller");
+const {
+  registerUser,
+  loginUser,
+  googleLoginUser,
+} = require("../controllers/auth.controller");
 
 // Register user
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.post("/google-login", googleLoginUser);
 
 module.exports = router;
