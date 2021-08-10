5831119d2f3c9d168f46a9531afa252b2754c3ee
yewyewXD <yewyew6933@gmail.com>
2021-08-10 13:22:08 +0200

    simplify arrow func

--- backend/utils/day.utils.js
+++ backend/utils/day.utils.js
@@ -1,5 +1,3 @@
 exports.ONE_DAY_IN_SEC = 24 * 60 * 60 * 1000;
 
-exports.getOneDayFromNow = () => {
-  return new Date().getTime() + ONE_DAY_IN_SEC;
-};
+exports.getOneDayFromNow = () => new Date().getTime() + ONE_DAY_IN_SEC;
