605c062990559b770b12962df2fef5d38f5763a9
yewyewXD <yewyew6933@gmail.com>
2021-08-11 10:34:22 +0200

    if has fetched once the recaptcha token, dont repeat

--- frontend/src/globalUI/Modal/components/ReCaptcha.jsx
+++ frontend/src/globalUI/Modal/components/ReCaptcha.jsx
@@ -1,17 +1,19 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import ReCAPTCHA from "react-google-recaptcha";
 
 const ReCaptcha = ({ isSubmittingForm, setReCaptchaToken }) => {
+  const [hasVerified, setHasVerified] = useState(false);
   const reCaptchaRef = useRef();
 
   async function getReCaptchaValue() {
     const token = await reCaptchaRef.current.executeAsync();
     setReCaptchaToken(token);
+    setHasVerified(true);
     reCaptchaRef.current.reset();
   }
 
   useEffect(() => {
-    if (isSubmittingForm) {
+    if (isSubmittingForm && !hasVerified) {
       getReCaptchaValue();
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
