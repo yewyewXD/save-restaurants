40cc813175425745503d95272907ca4000f1d229
yewyewXD <yewyew6933@gmail.com>
2021-08-12 09:35:17 +0200

    add func to toggle show navbar

--- frontend/src/pages/Home/components/Navbar.tsx
+++ frontend/src/pages/Home/components/Navbar.tsx
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Navbar = () => {
+  const [showNavbar, setShowNavbar] = useState(false);
+
+  function toggleShowNavbar() {
+    setShowNavbar((prevShow) => !prevShow);
+  }
+
   return (
     <nav className="flex items-center justify-between flex-wrap bg-green-600 p-6">
       <div className="flex items-center flex-shrink-0 text-white mr-6">
@@ -18,7 +24,10 @@ const Navbar = () => {
         </span>
       </div>
       <div className="block lg:hidden">
-        <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+        <button
+          onClick={toggleShowNavbar}
+          className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white"
+        >
           <svg
             className="fill-current h-3 w-3"
             viewBox="0 0 20 20"
@@ -29,7 +38,11 @@ const Navbar = () => {
           </svg>
         </button>
       </div>
-      <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+      <div
+        className={`${
+          showNavbar ? "block" : "hidden"
+        } w-full flex-grow lg:flex lg:items-center lg:w-auto`}
+      >
         <div className="text-sm lg:flex-grow">
           <a
             href="#responsive-header"
