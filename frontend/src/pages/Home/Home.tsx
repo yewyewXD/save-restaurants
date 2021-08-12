7fd8f56880c7c76f3d81b62dd9e74ae0fd9e5411
yewyewXD <yewyew6933@gmail.com>
2021-08-12 09:46:31 +0200

    remove old navbar, use new

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,31 +1,11 @@
 import React, { FC } from "react";
-import { useAuth } from "../../context/auth/AuthState";
 import HeroSection from "./components/HeroSection";
+import Navbar from "./components/Navbar";
 
 const Home: FC = () => {
-  const { userInfo, isLoggedIn, clearUserAuth } = useAuth();
   return (
     <main>
-      <nav className="h-12 border-b border-black">
-        <div className="my-2 h-full w-full flex justify-between items-center mx-5">
-          <div>
-            {isLoggedIn
-              ? `Welcome back, ${userInfo.username}!`
-              : "You are not logged in"}
-          </div>
-
-          {isLoggedIn && (
-            <button
-              onClick={clearUserAuth}
-              data-testid="register-btn"
-              className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-            >
-              Logout
-            </button>
-          )}
-        </div>
-      </nav>
-
+      <Navbar />
       <HeroSection />
     </main>
   );
