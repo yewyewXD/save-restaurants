e51090aaf3ca12e376b314397f4bb0e5cfb313fc
yewyewXD <yewyew6933@gmail.com>
2021-08-12 09:46:02 +0200

    ui adjustment

--- frontend/src/pages/Home/components/Navbar.tsx
+++ frontend/src/pages/Home/components/Navbar.tsx
@@ -8,8 +8,8 @@ const Navbar = () => {
   }
 
   return (
-    <nav className="flex items-center justify-between flex-wrap bg-green-600 p-6">
-      <div className="flex items-center flex-shrink-0 text-white mr-6">
+    <nav className="flex items-center justify-between bg-white flex-wrap p-6">
+      <div className="flex items-center flex-shrink-0 mr-14">
         <svg
           className="fill-current h-8 w-8 mr-2"
           width="54"
@@ -19,14 +19,12 @@ const Navbar = () => {
         >
           <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
         </svg>
-        <span className="font-semibold text-xl tracking-tight">
-          Tailwind CSS
-        </span>
+        <span className="font-semibold text-xl tracking-tight">Instantfly</span>
       </div>
-      <div className="block lg:hidden">
+      <div className="block md:hidden">
         <button
           onClick={toggleShowNavbar}
-          className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white"
+          className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400  hover:border-white"
         >
           <svg
             className="fill-current h-3 w-3"
@@ -41,34 +39,22 @@ const Navbar = () => {
       <div
         className={`${
           showNavbar ? "block" : "hidden"
-        } w-full flex-grow lg:flex lg:items-center lg:w-auto`}
+        } w-full flex-grow md:flex md:items-center md:w-auto`}
       >
-        <div className="text-sm lg:flex-grow">
+        <div className="text-sm md:flex-grow">
           <a
             href="#responsive-header"
-            className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+            className="block mt-4 md:inline-block md:mt-0 text-teal-200  mr-4"
           >
             Docs
-          </a>
-          <a
-            href="#responsive-header"
-            className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
-          >
-            Examples
-          </a>
-          <a
-            href="#responsive-header"
-            className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white"
-          >
-            Blog
           </a>
         </div>
         <div>
           <a
             href="/"
-            className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
+            className="transition duration-200 inline-block text-sm px-6 font-bold py-2 leading-none rounded mt-4 md:mt-0 bg-yellow-400 text-white hover:text-black hover:bg-white hover:border-yellow-400 border"
           >
-            Download
+            Sign in
           </a>
         </div>
       </div>
