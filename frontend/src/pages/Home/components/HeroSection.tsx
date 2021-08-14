3f6accad1341ad363e15a3924e041485851088cc
yewyewXD <yewyew6933@gmail.com>
2021-08-14 07:33:55 +0200

    rework hero

--- frontend/src/pages/Home/components/HeroSection.tsx
+++ frontend/src/pages/Home/components/HeroSection.tsx
@@ -1,35 +1,21 @@
 import React from "react";
-import { useModal } from "../../../context/modal/ModalState";
-import AuthModal from "../../../globalUI/Modal/components/AuthModal";
 
 const HeroSection = () => {
-  const { handleShowModal } = useModal();
-
-  function showLoginModal() {
-    handleShowModal(<AuthModal isLogin={true} />);
-  }
-
-  function showRegisterModal() {
-    handleShowModal(<AuthModal isLogin={false} />);
-  }
-
   return (
-    <section className="flex w-full h-screen justify-center items-center">
-      <div>
-        <h1>welcome to this website</h1>
-        <div className="flex">
-          <button
-            onClick={showLoginModal}
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"
-          >
-            Login
-          </button>
-          <button
-            data-testid="register-btn"
-            onClick={showRegisterModal}
-            className="ml-2 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"
-          >
-            Register
+    <section
+      className="flex w-full justify-center items-center"
+      style={{ height: "80vh" }}
+    >
+      <div className="grid grid-cols-5 container">
+        <div className="flex col-span-2 flex-col items-start">
+          <h1 className="leading-none text-6xl font-bold">Your Restaurant</h1>
+          <h1 className="leading-none text-6xl font-bold">Our Care</h1>
+          <div className="text-gray-500 my-4">
+            We've programmed our very own IT system for restaurant businesses to
+            go online effortlessly
+          </div>
+          <button className="bg-yellow-400 px-9 py-3 rounded text-white hover:bg-black transition duration-200 font-bold">
+            Get Free Forever
           </button>
         </div>
       </div>
