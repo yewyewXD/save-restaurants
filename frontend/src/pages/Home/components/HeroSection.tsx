362e127051e857cdfe16650e14e6711188860c9a
yewyewXD <yewyew6933@gmail.com>
2021-08-19 16:10:24 +0200

    responsive hero sec

--- frontend/src/pages/Home/components/HeroSection.tsx
+++ frontend/src/pages/Home/components/HeroSection.tsx
@@ -6,10 +6,14 @@ const HeroSection = () => {
       className="flex w-full justify-center items-center"
       style={{ height: "80vh" }}
     >
-      <div className="grid grid-cols-5 container">
-        <div className="flex col-span-2 flex-col items-start">
-          <h1 className="leading-none text-6xl font-bold">Your Restaurant</h1>
-          <h1 className="leading-none text-6xl font-bold">Our Care</h1>
+      <div className="grid md:grid-cols-5 grid-cols-1 container">
+        <div className="flex md:col-span-2 col-span-full flex-col md:items-start items-center md:text-left text-center">
+          <h1 className="leading-none md:text-6xl text-5xl font-bold">
+            Your Restaurant
+          </h1>
+          <h1 className="leading-none md:text-6xl text-5xl font-bold">
+            Our Care
+          </h1>
           <div className="text-gray-500 my-4">
             We've programmed our very own IT system for restaurant businesses to
             go online effortlessly
