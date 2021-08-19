b26593b6e3352f1243996028fb4e2b631778e557
yewyewXD <yewyew6933@gmail.com>
2021-08-19 15:48:12 +0200

    remove TestimonialSection for now

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -4,16 +4,16 @@ import GetStartedSection from "./components/GetStartedSection";
 import HeroSection from "./components/HeroSection";
 import IntegrationSection from "./components/IntegrationSection";
 import Navbar from "./components/Navbar";
-import TestimonialSection from "./components/TestimonialSection";
+// import TestimonialSection from "./components/TestimonialSection";
 
 const Home: FC = () => {
   return (
     <main>
       <Navbar />
       <HeroSection />
-      <TestimonialSection />
       <CountUpSection />
       <IntegrationSection />
+      {/* <TestimonialSection /> */}
       <GetStartedSection />
     </main>
   );
