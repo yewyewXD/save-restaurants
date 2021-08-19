87e9954d9546e23bd71fd8c3175fd90e37abbd8b
yewyewXD <yewyew6933@gmail.com>
2021-08-19 15:43:20 +0200

    init testimonial section

---
+++ frontend/src/pages/Home/components/TestimonialSection.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import {
+  CarouselProvider,
+  Slider,
+  Slide,
+  ButtonBack,
+  ButtonNext,
+} from "pure-react-carousel";
+import "pure-react-carousel/dist/react-carousel.es.css";
+
+const TestimonialSection = () => {
+  return (
+    <section className="text-center w-full py-16 bg-yellow-400">
+      <CarouselProvider
+        naturalSlideWidth={100}
+        naturalSlideHeight={20}
+        totalSlides={3}
+        infinite={true}
+      >
+        <Slider>
+          <Slide index={0}>I am the first Slide.</Slide>
+          <Slide index={1}>I am the second Slide.</Slide>
+          <Slide index={2}>I am the third Slide.</Slide>
+        </Slider>
+        <ButtonBack>Back</ButtonBack>
+        <ButtonNext>Next</ButtonNext>
+      </CarouselProvider>
+    </section>
+  );
+};
+
+export default TestimonialSection;
