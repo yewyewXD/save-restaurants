b5add241607d13fe9c5e5cb44187c59a81c48bd5
yewyewXD <yewyew6933@gmail.com>
2021-08-19 15:43:34 +0200

    add testimonial section

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -4,12 +4,14 @@ import GetStartedSection from "./components/GetStartedSection";
 import HeroSection from "./components/HeroSection";
 import IntegrationSection from "./components/IntegrationSection";
 import Navbar from "./components/Navbar";
+import TestimonialSection from "./components/TestimonialSection";
 
 const Home: FC = () => {
   return (
     <main>
       <Navbar />
       <HeroSection />
+      <TestimonialSection />
       <CountUpSection />
       <IntegrationSection />
       <GetStartedSection />
