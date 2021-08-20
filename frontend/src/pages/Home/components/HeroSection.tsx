812d58bfb7a3e4f22d0103b494791d061fe0ac8a
yewyewXD <yewyew6933@gmail.com>
2021-08-20 18:55:22 +0200

    show register modal on click cta

--- frontend/src/pages/Home/components/HeroSection.tsx
+++ frontend/src/pages/Home/components/HeroSection.tsx
@@ -1,6 +1,14 @@
 import React from "react";
+import { useModal } from "../../../context/modal/ModalState";
+import AuthModal from "../../../globalUI/Modal/components/AuthModal";
 
 const HeroSection = () => {
+  const { handleShowModal } = useModal();
+
+  function showRegisterModal() {
+    handleShowModal(<AuthModal isLogin={false} />);
+  }
+
   return (
     <section
       className="flex w-full justify-center items-center"
@@ -18,7 +26,10 @@ const HeroSection = () => {
             We've programmed our very own IT system for restaurant businesses to
             go online effortlessly
           </div>
-          <button className="bg-yellow-400 px-9 py-3 rounded text-black hover:text-white hover:bg-black transition duration-200 font-bold">
+          <button
+            className="bg-yellow-400 px-9 py-3 rounded text-black hover:text-white hover:bg-black transition duration-200 font-bold"
+            onClick={showRegisterModal}
+          >
             Get Free Forever
           </button>
         </div>
