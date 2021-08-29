14881f1042a9ce2eae56361502e5c4e916f09414
yewyewXD <yewyew6933@gmail.com>
2021-08-29 15:38:54 +0200

    setup route for customer site

--- backend/server.js
+++ backend/server.js
@@ -26,8 +26,10 @@ app.set("view engine", "ejs");
 // routes
 const userRoute = require("./routes/user.route");
 const authRoute = require("./routes/auth.route");
+const siteRoute = require("./routes/site.route");
 app.use("/api/user", userRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/site", siteRoute);
 
 // port
 const PORT = process.env.PORT || 5000;
