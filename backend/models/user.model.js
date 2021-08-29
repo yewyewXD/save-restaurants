4eb6124bf5a7e5dc38e5fec968228b80c2943ad8
yewyewXD <yewyew6933@gmail.com>
2021-08-29 08:21:25 +0200

    add siteId to user model

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -20,6 +20,10 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: false,
     },
+    siteId: {
+      type: String,
+      required: false,
+    },
   },
   { timestamps: true }
 );
