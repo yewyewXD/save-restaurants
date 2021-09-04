d07b49908f1eadd32a4b9e7866dfff1bf1265b58
yewyewXD <yewyew6933@gmail.com>
2021-09-04 09:12:33 +0200

    remove everything about ejs

--- backend/routes/site.route.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/", async (req, res) => {
-  res.render("site");
-});
-
-module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -15,21 +15,11 @@ app.use(compression());
 app.use(express.json());
 app.use(cookieParser());
 
-// ejs middleware
-app.use(express.static("assets"));
-app.use("/css", express.static(__dirname + "assets/css"));
-app.use("/img", express.static(__dirname + "assets/img"));
-app.use("/js", express.static(__dirname + "assets/js"));
-app.set("views", "./views");
-app.set("view engine", "ejs");
-
 // routes
 const userRoute = require("./routes/user.route");
 const authRoute = require("./routes/auth.route");
-const siteRoute = require("./routes/site.route");
 app.use("/api/user", userRoute);
 app.use("/api/auth", authRoute);
-app.use("/api/site", siteRoute);
 
 // port
 const PORT = process.env.PORT || 5000;
