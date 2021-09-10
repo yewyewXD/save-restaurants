9e83dd50d05dc8059a029c65cae9152b0ee43435
yewyewXD <yewyew6933@gmail.com>
2021-09-10 14:43:00 +0200

    give a fixed size for navSidebar

--- frontend/src/pages/Dashboard/components/NavSidebar.tsx
+++ frontend/src/pages/Dashboard/components/NavSidebar.tsx
@@ -9,27 +9,26 @@ const NavSidebar: FC<Props> = ({ currentTab, setCurrentTab }) => {
   const sidebarTabs = [{ name: "Profile" }, { name: "Sites" }];
 
   return (
-    <div
-      className="p-4 h-screen fixed left-0 top-0 bg-black text-white"
-      style={{ width: "20%" }}
-    >
-      <div>Eatery</div>
+    <div className="relative h-screen w-60">
+      <div className="p-4 h-screen fixed left-0 top-0 bg-black text-white w-60">
+        <div>Eatery</div>
 
-      <div className="mt-8">
-        {sidebarTabs.map((tab, index) => (
-          <div
-            onClick={() => {
-              setCurrentTab(tab.name);
-            }}
-            className={`mb-3 border rounded-xl p-4 flex items-center ${
-              currentTab === tab.name ? "border-red-600" : ""
-            }`}
-            key={`sidebar-${index}`}
-          >
-            <i className="icon-inno icon-inno_clock mr-3" />
-            {tab.name}
-          </div>
-        ))}
+        <div className="mt-8">
+          {sidebarTabs.map((tab, index) => (
+            <div
+              onClick={() => {
+                setCurrentTab(tab.name);
+              }}
+              className={`mb-3 border rounded-xl p-4 flex items-center ${
+                currentTab === tab.name ? "border-red-600" : ""
+              }`}
+              key={`sidebar-${index}`}
+            >
+              <i className="icon-inno icon-inno_clock mr-3" />
+              {tab.name}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
