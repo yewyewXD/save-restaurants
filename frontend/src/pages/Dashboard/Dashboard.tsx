6e14893cc97edb28bdbf0bcb1c8b4019ad6fa78f
yewyewXD <yewyew6933@gmail.com>
2021-09-10 14:44:40 +0200

    setup siteAll tab

--- frontend/src/pages/Dashboard/Dashboard.tsx
+++ frontend/src/pages/Dashboard/Dashboard.tsx
@@ -1,13 +1,15 @@
 import React, { FC, useState } from "react";
 import NavSidebar from "./components/NavSidebar";
+import SiteAll from "./components/SiteAll";
 
 const Dashboard: FC = () => {
   const [currentTab, setCurrentTab] = useState("Sites");
 
   return (
-    <>
+    <div className="flex">
       <NavSidebar currentTab={currentTab} setCurrentTab={setCurrentTab} />
-    </>
+      <SiteAll />
+    </div>
   );
 };
 
---
+++ frontend/src/pages/Dashboard/components/SiteAll.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SiteAll = () => {
+  return (
+    <div className="flex-grow">
+      <div>testing only</div>
+    </div>
+  );
+};
+
+export default SiteAll;
