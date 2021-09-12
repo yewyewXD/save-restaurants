6c3ed5c4cdab694a4214ee4088ba6a95b27e03f9
yewyewXD <yewyew6933@gmail.com>
2021-09-12 09:39:33 +0200

    show sites dynamically

--- frontend/src/pages/Dashboard/components/SiteAll.tsx
+++ frontend/src/pages/Dashboard/components/SiteAll.tsx
@@ -1,9 +1,41 @@
 import React from "react";
 
 const SiteAll = () => {
+  const sites = [
+    {
+      name: "site 1",
+      updated: new Date().toDateString(),
+      image: null,
+    },
+    {
+      name: "site 1",
+      updated: new Date().toDateString(),
+      image: null,
+    },
+    {
+      name: "site 1",
+      updated: new Date().toDateString(),
+      image: null,
+    },
+  ];
   return (
     <>
       <div className="leading-none text-2xl">All My Sites</div>
+
+      <div className="mt-6 grid grid-cols-3 gap-16 auto-rows-auto">
+        {sites.map((site, index) => (
+          <div
+            key={`site-${index}`}
+            className="border h-50 w-50 flex justify-center items-center flex-col border-red-600"
+          >
+            <div className="h-28 w-full bg-black"></div>
+            <div className="p-3 flex justify-center items-center flex-col">
+              <div>{site.name}</div>
+              <div>{site.updated}</div>
+            </div>
+          </div>
+        ))}
+      </div>
     </>
   );
 };
