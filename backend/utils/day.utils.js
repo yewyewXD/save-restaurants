1ab4135c01ceec6dbb611ad88c6417f8dacc7c74
yewyewXD <yewyew6933@gmail.com>
2021-09-19 18:19:26 +0200

    setup func to get current time in ms and utc

--- backend/utils/day.utils.js
+++ backend/utils/day.utils.js
@@ -1,5 +1,12 @@
-exports.ONE_DAY_IN_SEC = 24 * 60 * 60 * 1000;
-exports.HALF_DAY_IN_SEC = 12 * 60 * 60 * 1000;
-exports.SIX_HOURS_IN_SEC = 6 * 60 * 60 * 1000;
+exports.SIX_HOURS_IN_SEC = 21600000;
 
-exports.getOneDayFromNow = () => new Date().getTime() + ONE_DAY_IN_SEC;
+exports.get6HFromNow = () => new Date().getTime() + SIX_HOURS_IN_SEC;
+
+exports.getFull6HFromNow = () => {
+  const currentDate = new Date();
+  currentDate.setHours(currentDate.getHours() + 6);
+  return {
+    ms: currentDate.getTime(),
+    utc: currentDate.toUTCString(),
+  };
+};
