08178978c9f483d754049bb45408cc98a28702d6
yewyewXD <yewyew6933@gmail.com>
2021-09-19 18:35:11 +0200

    comment unused site context

--- frontend/src/context/site/SiteReducer.js
+++ frontend/src/context/site/SiteReducer.js
@@ -1,23 +1,23 @@
-const AuthReducer = (state, { type, payload }) => {
-  switch (type) {
-    case "SAVE_USER_AUTH":
-      return {
-        userInfo: payload.userData,
-        isLoggedIn: true,
-      };
+// const AuthReducer = (state, { type, payload }) => {
+//   switch (type) {
+//     case "SAVE_USER_AUTH":
+//       return {
+//         userInfo: payload.userData,
+//         isLoggedIn: true,
+//       };
 
-    case "CLEAR_USER_AUTH":
-      return {
-        userInfo: {
-          username: "",
-          email: "",
-        },
-        isLoggedIn: false,
-      };
+//     case "CLEAR_USER_AUTH":
+//       return {
+//         userInfo: {
+//           username: "",
+//           email: "",
+//         },
+//         isLoggedIn: false,
+//       };
 
-    default:
-      return state;
-  }
-};
+//     default:
+//       return state;
+//   }
+// };
 
-export default AuthReducer;
+// export default AuthReducer;
--- frontend/src/context/site/SiteState.js
+++ frontend/src/context/site/SiteState.js
@@ -1,99 +1,99 @@
-import React, { createContext, useReducer, useEffect, useContext } from "react";
-import { logoutUser } from "../../api/auth.api";
-import { getUserMe } from "../../api/user.api";
-import AuthReducer from "./AuthReducer";
-import serialize from "serialize-javascript";
+// import React, { createContext, useReducer, useEffect, useContext } from "react";
+// import { logoutUser } from "../../api/auth.api";
+// import { getUserMe } from "../../api/user.api";
+// import AuthReducer from "./AuthReducer";
+// import serialize from "serialize-javascript";
 
-const initialState = {
-  userInfo: {
-    username: "",
-    email: "",
-  },
-  isLoggedIn: false,
-};
+// const initialState = {
+//   userInfo: {
+//     username: "",
+//     email: "",
+//   },
+//   isLoggedIn: false,
+// };
 
-export const AuthContext = createContext(initialState);
+// export const AuthContext = createContext(initialState);
 
-export const AuthContextProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(AuthReducer, initialState, () => {
-    if (typeof window !== "undefined") {
-      const storedAuth = localStorage.getItem("AuthState");
-      return storedAuth ? JSON.parse(storedAuth) : initialState;
-    } else {
-      return initialState;
-    }
-  });
+// export const AuthContextProvider = ({ children }) => {
+//   const [state, dispatch] = useReducer(AuthReducer, initialState, () => {
+//     if (typeof window !== "undefined") {
+//       const storedAuth = localStorage.getItem("AuthState");
+//       return storedAuth ? JSON.parse(storedAuth) : initialState;
+//     } else {
+//       return initialState;
+//     }
+//   });
 
-  // save to localStorage
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      localStorage.setItem("AuthState", serialize(state, { isJSON: true }));
-    }
-  }, [state]);
+//   // save to localStorage
+//   useEffect(() => {
+//     if (typeof window !== "undefined") {
+//       localStorage.setItem("AuthState", serialize(state, { isJSON: true }));
+//     }
+//   }, [state]);
 
-  useEffect(() => {
-    // on close browser remove local auth
-    function removeLocalAuth() {
-      localStorage.removeItem("AuthState");
-    }
-    if (typeof window !== "undefined") {
-      window.addEventListener("unload", removeLocalAuth);
-    }
+//   useEffect(() => {
+//     // on close browser remove local auth
+//     function removeLocalAuth() {
+//       localStorage.removeItem("AuthState");
+//     }
+//     if (typeof window !== "undefined") {
+//       window.addEventListener("unload", removeLocalAuth);
+//     }
 
-    // auto login on load
-    async function handleAutoLogin() {
-      try {
-        const res = await getUserMe();
-        saveUserAuth(res.data);
-      } catch {}
-    }
-    const storedAuth = localStorage.getItem("AuthState");
-    if (!JSON.parse(storedAuth).isLoggedIn) {
-      handleAutoLogin();
-    }
+//     // auto login on load
+//     async function handleAutoLogin() {
+//       try {
+//         const res = await getUserMe();
+//         saveUserAuth(res.data);
+//       } catch {}
+//     }
+//     const storedAuth = localStorage.getItem("AuthState");
+//     if (!JSON.parse(storedAuth).isLoggedIn) {
+//       handleAutoLogin();
+//     }
 
-    // clean up remove local auth function
-    return () => {
-      if (typeof window !== "undefined") {
-        window.removeEventListener("unload", removeLocalAuth);
-      }
-    };
-  }, []);
+//     // clean up remove local auth function
+//     return () => {
+//       if (typeof window !== "undefined") {
+//         window.removeEventListener("unload", removeLocalAuth);
+//       }
+//     };
+//   }, []);
 
-  // actions
-  function saveUserAuth(userData) {
-    dispatch({
-      type: "SAVE_USER_AUTH",
-      payload: { userData },
-    });
-  }
+//   // actions
+//   function saveUserAuth(userData) {
+//     dispatch({
+//       type: "SAVE_USER_AUTH",
+//       payload: { userData },
+//     });
+//   }
 
-  async function clearUserAuth() {
-    await logoutUser();
+//   async function clearUserAuth() {
+//     await logoutUser();
 
-    dispatch({
-      type: "CLEAR_USER_AUTH",
-      payload: null,
-    });
-  }
+//     dispatch({
+//       type: "CLEAR_USER_AUTH",
+//       payload: null,
+//     });
+//   }
 
-  return (
-    <AuthContext.Provider
-      value={{
-        userInfo: state.userInfo,
-        isLoggedIn: state.isLoggedIn,
-        saveUserAuth,
-        clearUserAuth,
-      }}
-    >
-      {children}
-    </AuthContext.Provider>
-  );
-};
+//   return (
+//     <AuthContext.Provider
+//       value={{
+//         userInfo: state.userInfo,
+//         isLoggedIn: state.isLoggedIn,
+//         saveUserAuth,
+//         clearUserAuth,
+//       }}
+//     >
+//       {children}
+//     </AuthContext.Provider>
+//   );
+// };
 
-export function useAuth() {
-  const { userInfo, isLoggedIn, saveUserAuth, clearUserAuth } =
-    useContext(AuthContext);
+// export function useAuth() {
+//   const { userInfo, isLoggedIn, saveUserAuth, clearUserAuth } =
+//     useContext(AuthContext);
 
-  return { userInfo, isLoggedIn, saveUserAuth, clearUserAuth };
-}
+//   return { userInfo, isLoggedIn, saveUserAuth, clearUserAuth };
+// }
