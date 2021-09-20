e14577027843cb4d49dc9537278682327d66b809
yewyewXD <yewyew6933@gmail.com>
2021-09-20 17:18:33 +0200

    check hasAuth based on path

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,9 @@
-import React, { Suspense, lazy } from "react";
+import React, { Suspense, lazy, useState, useEffect } from "react";
 import { AuthContextProvider } from "./context/auth/AuthState";
 import { ModalProvider } from "./context/modal/ModalState";
 import { NotificationProvider } from "./context/notification/NotificationState";
-import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import { Switch, Route, Redirect, useLocation } from "react-router-dom";
+import { getStorageAuthState } from "./utils/authState.utils";
 
 const Home = lazy(() => import("./pages/Home/Home"));
 const Dashboard = lazy(() => import("./pages/Dashboard/Dashboard"));
@@ -14,28 +15,39 @@ function HandleRedirect(path: string) {
 }
 
 function App() {
+  const [hasAuth, setHasAuth] = useState(false);
+
+  const location = useLocation();
+
+  useEffect(() => {
+    console.log("checked");
+
+    const { isLoggedIn } = getStorageAuthState();
+    if (isLoggedIn && !hasAuth) {
+      setHasAuth(true);
+    } else if (!isLoggedIn && hasAuth) {
+      setHasAuth(false);
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [location]);
+
   return (
-    <BrowserRouter>
-      <AuthContextProvider>
-        <NotificationProvider>
-          <ModalProvider>
-            <Switch>
-              <Suspense fallback="Loading...">
-                <Route path="/" component={Home} exact />
-                <Route path="/home" render={() => HandleRedirect("/")} exact />
-                <Route path="/dashboard" component={Dashboard} exact />
-                <Route path="/dashboard/sites/new" component={SiteNew} exact />
-                <Route
-                  path="/dashboard/sites/:id"
-                  component={SiteSingle}
-                  exact
-                />
-              </Suspense>
-            </Switch>
-          </ModalProvider>
-        </NotificationProvider>
-      </AuthContextProvider>
-    </BrowserRouter>
+    <AuthContextProvider>
+      <NotificationProvider>
+        <ModalProvider>
+          <Switch>
+            <Suspense fallback="Loading...">
+              <Route path="/" component={Home} exact />
+              <Route path="/home" render={() => HandleRedirect("/")} exact />
+              <Route path="/dashboard" component={Dashboard} exact />
+              <Route path="/dashboard/sites/new" component={SiteNew} exact />
+              <Route path="/dashboard/sites/:id" component={SiteSingle} exact />
+            </Suspense>
+          </Switch>
+        </ModalProvider>
+      </NotificationProvider>
+    </AuthContextProvider>
   );
 }
 
