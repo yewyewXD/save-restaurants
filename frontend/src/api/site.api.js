4891161ffb8e0307c9a76e7085165c8c63b52b95
yewyewXD <yewyew6933@gmail.com>
2021-09-20 12:59:19 +0200

    site api deprecated for now

--- frontend/src/api/site.api.js
+++ frontend/src/api/site.api.js
@@ -1,29 +1,29 @@
-import { request } from "@octokit/request";
+// import { request } from "@octokit/request";
 
-const authRequest = request.defaults({
-  headers: {
-    authorization: `token ${process.env.REACT_APP_GITHUB_ACCESS_TOKEN}`,
-  },
-});
+// const authRequest = request.defaults({
+//   headers: {
+//     authorization: `token ${process.env.REACT_APP_GITHUB_ACCESS_TOKEN}`,
+//   },
+// });
 
-export function createRepo({ repoName }) {
-  const authRequest = request.defaults({
-    headers: {
-      authorization: `token ${process.env.REACT_APP_GITHUB_ACCESS_TOKEN}`,
-    },
-  });
-  return authRequest("POST /user/repos", {
-    name: repoName,
-    private: true,
-  });
-}
+// export function createRepo({ repoName }) {
+//   const authRequest = request.defaults({
+//     headers: {
+//       authorization: `token ${process.env.REACT_APP_GITHUB_ACCESS_TOKEN}`,
+//     },
+//   });
+//   return authRequest("POST /user/repos", {
+//     name: repoName,
+//     private: true,
+//   });
+// }
 
-export function addRepoContent({ repoName, fileText, fileName }) {
-  return authRequest(`PUT /repos/yewyewXD/${repoName}/contents/${fileName}`, {
-    owner: "yewyewXD",
-    repo: repoName,
-    path: fileName,
-    message: new Date(),
-    content: btoa(fileText),
-  });
-}
+// export function addRepoContent({ repoName, fileText, fileName }) {
+//   return authRequest(`PUT /repos/yewyewXD/${repoName}/contents/${fileName}`, {
+//     owner: "yewyewXD",
+//     repo: repoName,
+//     path: fileName,
+//     message: new Date(),
+//     content: btoa(fileText),
+//   });
+// }
