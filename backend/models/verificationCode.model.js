7277034b23ee70b7bc03253cece32b62c2100ca1
yewyewXD <yewyew6933@gmail.com>
2021-09-21 10:31:20 +0200

    fix VerificationCodes auto expire

--- backend/models/verificationCode.model.js
+++ backend/models/verificationCode.model.js
@@ -1,24 +1,16 @@
 const mongoose = require("mongoose");
-const { get30DaysFromNow } = require("../utils/day.utils");
 
-const verificationCodeSchema = new mongoose.Schema(
-  {
-    user: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Users",
-      required: true,
-    },
-    code: {
-      type: String,
-      required: true,
-    },
-    expiry: {
-      type: Date,
-      default: Date.now(),
-      expires: 2592000, // expires after 30 days
-    },
+const verificationCodeSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Users",
+    required: true,
   },
-  { timestamps: true }
-);
+  code: {
+    type: String,
+    required: true,
+  },
+  expiry: { type: Date, index: { unique: true, expires: "30d" } },
+});
 
-module.exports = mongoose.model("VerificationCode", verificationCodeSchema);
+module.exports = mongoose.model("VerificationCodes", verificationCodeSchema);
