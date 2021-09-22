e8539117d1f639ea6b9702d3b426b1e5be23be47
yewyewXD <yewyew6933@gmail.com>
2021-09-22 19:07:25 +0200

    remove private route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,6 @@ import { AuthContextProvider } from "./context/auth/AuthState";
 import { ModalProvider } from "./context/modal/ModalState";
 import { NotificationProvider } from "./context/notification/NotificationState";
 import { Switch, Route, Redirect } from "react-router-dom";
-import { PrivateRoute } from "./utils/auth.utils";
 
 const Home = lazy(() => import("./pages/Home/Home"));
 const Dashboard = lazy(() => import("./pages/Dashboard/Dashboard"));
@@ -25,17 +24,9 @@ function App() {
             <Suspense fallback="Loading...">
               <Route path="/" component={Home} exact />
               <Route path="/home" render={() => HandleRedirect("/")} exact />
-              <PrivateRoute path="/dashboard" component={Dashboard} exact />
-              <PrivateRoute
-                path="/dashboard/sites/new"
-                component={SiteNew}
-                exact
-              />
-              <PrivateRoute
-                path="/dashboard/sites/:id"
-                component={SiteSingle}
-                exact
-              />
+              <Route path="/dashboard" component={Dashboard} exact />
+              <Route path="/dashboard/sites/new" component={SiteNew} exact />
+              <Route path="/dashboard/sites/:id" component={SiteSingle} exact />
               <Route path="/login" component={Login} exact />
             </Suspense>
           </Switch>
