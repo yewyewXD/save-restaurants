9f8ff755ce6abbd5d5a8f9fdbc5f27bb3706d4ce
yewyewXD <yewyew6933@gmail.com>
2021-09-22 16:04:47 +0200

    remove deprecated mongodb options

--- backend/config/database.js
+++ backend/config/database.js
@@ -5,8 +5,6 @@ const connectDB = async () => {
     const mongoDb = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useFindAndModify: false,
-      useCreateIndex: true,
     });
 
     console.log(`MongoDB Connected: ${mongoDb.connection.host}`);
