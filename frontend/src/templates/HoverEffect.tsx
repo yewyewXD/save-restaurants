03b99c91074c2200acf4e6da7e98eec229167d86
yewyewXD <yewyew6933@gmail.com>
2021-10-12 08:44:10 +0200

    setup hover ebitable effect

---
+++ frontend/src/templates/HoverEffect.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+interface Props {
+  elementName: string;
+}
+
+const HoverEffect: React.FC<Props> = ({ elementName }) => {
+  return (
+    <div className="justify-start items-start absolute h-full w-full border-green-400 border-dashed border-4">
+      <span className="px-5 bg-black text-white">{elementName}</span>
+    </div>
+  );
+};
+
+export default HoverEffect;
