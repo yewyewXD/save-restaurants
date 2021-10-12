4368ad1aca2e26143438ccf954778b52c1978a69
yewyewXD <yewyew6933@gmail.com>
2021-10-12 09:14:31 +0200

    fix positionand bg  of element name

--- frontend/src/templates/HoverEffect.tsx
+++ frontend/src/templates/HoverEffect.tsx
@@ -6,8 +6,10 @@ interface Props {
 
 const HoverEffect: React.FC<Props> = ({ elementName }) => {
   return (
-    <div className="HoverEffect justify-start items-start absolute h-full w-full border-green-400 border-dashed border-4">
-      <span className="px-5 bg-black text-white">{elementName}</span>
+    <div className="HoverEffect flex justify-start items-start absolute h-full w-full border-green-400 border-dashed border-4">
+      <span className="text-xs px-5 py-1 bg-green-800 text-white absolute -top-7 -left-1">
+        {elementName}
+      </span>
     </div>
   );
 };
