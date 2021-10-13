8b22dac902e4c79fb0505c11d0a7cd5b7d73ab79
yewyewXD <yewyew6933@gmail.com>
2021-10-13 10:22:11 +0200

    setup custom side menu

--- frontend/src/globalUI/Site/SideMenu.tsx
+++ frontend/src/globalUI/Site/SideMenu.tsx
@@ -1,50 +1,32 @@
-import React, { useState } from "react";
-import { slide as Menu } from "react-burger-menu";
-import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
-import "react-tabs/style/react-tabs.css";
+import React from "react";
 
-const SideMenu = () => {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+interface IProps {
+  isOpened: boolean;
+  onCloseMenu: () => void;
+}
 
+const SideMenu: React.FC<IProps> = ({ isOpened, onCloseMenu }) => {
   return (
     <>
-      <Menu
-        right={true}
-        onStateChange={({ isOpen }) => {
-          setIsSidebarOpen(isOpen);
-        }}
-        customBurgerIcon={
-          isSidebarOpen ? (
-            false
-          ) : (
-            <i className="icon-inno icon-inno_edit text-white" />
-          )
-        }
-      >
-        <Tabs>
-          <TabList>
-            <Tab>Title 1</Tab>
-            <Tab>Title 2</Tab>
-          </TabList>
+      {isOpened && (
+        <div className="pointer-events-none bg-black w-full fixed h-full z-40 bg-opacity-30"></div>
+      )}
 
-          <TabPanel>
-            <div className="flex flex-col">
-              <a id="home" href="/">
-                Home
-              </a>
-              <a id="about" href="/about">
-                About
-              </a>
-              <a id="contact" href="/contact">
-                Contact
-              </a>
-            </div>
-          </TabPanel>
-          <TabPanel>
-            <h2>Any content 2</h2>
-          </TabPanel>
-        </Tabs>
-      </Menu>
+      <div
+        className={`h-screen w-72 transition duration-200 transform translate-x-${
+          isOpened ? "0" : "72"
+        } fixed top-0 right-0 z-50`}
+      >
+        <div className="relative w-full h-full p-6 pt-10 bg-white">
+          <span
+            className="absolute top-3 right-3 cursor-pointer"
+            onClick={onCloseMenu}
+          >
+            &#10005;
+          </span>
+          testing
+        </div>
+      </div>
     </>
   );
 };
