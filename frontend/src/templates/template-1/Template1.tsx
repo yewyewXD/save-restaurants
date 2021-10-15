f5573f2e305d23608cb2d56d79f1fda60d0fe7b5
yewyewXD <yewyew6933@gmail.com>
2021-10-15 18:51:22 +0200

    move header to separate component

--- frontend/src/templates/template-1/Template1.tsx
+++ frontend/src/templates/template-1/Template1.tsx
@@ -1,23 +1,11 @@
-import React, { FC, useEffect, useState } from "react";
-import Scrollspy from "react-scrollspy";
+import React, { FC, useState } from "react";
 import HoverEffect from "../../globalUI/Site/HoverEffect";
 import SideMenu from "../../globalUI/Site/SideMenu";
+import HeaderSection from "./components/HeaderSection";
 import MenuSection from "./components/MenuSection";
 
 const Template1: FC = () => {
   const [menuIsOpened, setMenuIsOpened] = useState(false);
-  const [navBg, setNavBg] = useState("bg-white");
-  useEffect(() => {
-    function scrollListener() {
-      const navBgClass = window.scrollY < 1 ? "bg-white" : "bg-primary z-30";
-      setNavBg(navBgClass);
-    }
-
-    document.addEventListener("scroll", scrollListener);
-    return () => {
-      document.removeEventListener("scroll", scrollListener);
-    };
-  }, [navBg]);
 
   const navItems = [
     { name: "logo", link: "home-section", isLogo: true },
@@ -46,43 +34,7 @@ const Template1: FC = () => {
       />
 
       {/* Navbar */}
-      <div className="relative h-16">
-        <header
-          className={`fixed flex justify-center items-center h-16 w-full top-0 ${navBg}`}
-        >
-          <div className="borderOnHover w-full">
-            <HoverEffect
-              onClick={handleOpenMenu}
-              elementName={"Navigation bar"}
-              showTextInner={true}
-            />
-            <Scrollspy
-              className="w-full grid grid-cols-5 text-center justify-center  h-16"
-              items={navItems.map((navItem) => navItem.link)}
-              currentClassName="underline"
-            >
-              {navItems.map((navItem) => (
-                <li
-                  key={`navItem-${navItem.link}`}
-                  className="flex justify-center items-center h-16"
-                >
-                  <a className="py-6" href={`#${navItem.link}`}>
-                    {navItem.isLogo ? (
-                      <img
-                        src="/images/netlify.png"
-                        alt=""
-                        style={{ maxWidth: "40px" }}
-                      />
-                    ) : (
-                      navItem.name
-                    )}
-                  </a>
-                </li>
-              ))}
-            </Scrollspy>
-          </div>
-        </header>
-      </div>
+      <HeaderSection handleOpenMenu={handleOpenMenu} />
 
       {/* Hero */}
       <section
---
+++ frontend/src/templates/template-1/components/HeaderSection.tsx
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from "react";
+import HoverEffect from "../../../globalUI/Site/HoverEffect";
+import Scrollspy from "react-scrollspy";
+
+interface IProps {
+  handleOpenMenu: () => void;
+}
+
+const HeaderSection: React.FC<IProps> = ({ handleOpenMenu }) => {
+  const [navBg, setNavBg] = useState("bg-white");
+  useEffect(() => {
+    function scrollListener() {
+      const navBgClass = window.scrollY < 1 ? "bg-white" : "bg-primary z-30";
+      setNavBg(navBgClass);
+    }
+
+    document.addEventListener("scroll", scrollListener);
+    return () => {
+      document.removeEventListener("scroll", scrollListener);
+    };
+  }, [navBg]);
+
+  const navItems = [
+    { name: "logo", link: "home-section", isLogo: true },
+    { name: "About Us", link: "about-section" },
+    { name: "Menu", link: "menu-section" },
+    { name: "Contact", link: "contact-section" },
+  ];
+
+  return (
+    <div className="relative h-16">
+      <header
+        className={`fixed flex justify-center items-center h-16 w-full top-0 ${navBg}`}
+      >
+        <nav className="borderOnHover w-full">
+          <HoverEffect
+            onClick={handleOpenMenu}
+            elementName={"Navigation bar"}
+            showTextInner={true}
+          />
+          <Scrollspy
+            className="w-full grid grid-cols-5 text-center justify-center  h-16"
+            items={navItems.map((navItem) => navItem.link)}
+            currentClassName="underline"
+          >
+            {navItems.map((navItem) => (
+              <li
+                key={`navItem-${navItem.link}`}
+                className="flex justify-center items-center h-16"
+              >
+                <a className="py-6" href={`#${navItem.link}`}>
+                  {navItem.isLogo ? (
+                    <img
+                      src="/images/netlify.png"
+                      alt=""
+                      style={{ maxWidth: "40px" }}
+                    />
+                  ) : (
+                    navItem.name
+                  )}
+                </a>
+              </li>
+            ))}
+          </Scrollspy>
+        </nav>
+      </header>
+    </div>
+  );
+};
+
+export default HeaderSection;
