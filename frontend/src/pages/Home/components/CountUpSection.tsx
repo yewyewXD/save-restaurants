e779ef45f6df4fc3c616875caff9ec3002693071
yewyewXD <yewyew6933@gmail.com>
2021-10-22 20:12:52 +0200

    add icons to each countup

--- frontend/src/pages/Home/components/CountUpSection.tsx
+++ frontend/src/pages/Home/components/CountUpSection.tsx
@@ -7,34 +7,38 @@ const CountUpSection = () => {
   const isVisible = useOnVisible(sectionRef);
   const progressions = useMemo(() => {
     return [
-      { id: 1, name: "Websites Created", count: 100 },
       {
-        id: 2,
-        name: "Orders Delivered",
-        count: 100,
+        id: 1,
+        name: "Websites Created",
+        count: 673,
+        image: "/images/web-icon.png",
       },
       {
+        id: 2,
+        name: "Orders Processed",
+        count: 2571,
+        image: "/images/order-icon.png",
+      },
+
+      {
         id: 3,
-        name: "Leads Generated",
-        count: 100,
+        name: "Reviews Generated",
+        count: 400,
+        image: "/images/review-icon.png",
       },
       {
         id: 4,
-        name: "Reviews Generated",
-        count: 100,
-      },
-      {
-        id: 5,
         name: "Cost Saved",
-        count: 100,
+        count: 20400,
+        image: "/images/cost-icon.png",
       },
     ];
   }, []);
 
   return (
-    <section className="text-center w-full py-16 bg-yellow-400">
+    <section className="text-center w-full py-16 bg-primary">
       <div
-        className="container mx-auto grid grid-cols-5 gap-7"
+        className="container mx-auto grid grid-cols-4 gap-7"
         ref={sectionRef}
       >
         {progressions
@@ -44,8 +48,11 @@ const CountUpSection = () => {
               className="flex justify-center items-center flex-col text-center"
               key={progression.id}
             >
+              {progression?.image && (
+                <img src={progression.image} alt="" className="w-12 mb-3" />
+              )}
               {isVisible && (
-                <div className="font-bold text-5xl mb-5">
+                <div className="font-semibold text-6xl mb-3">
                   {progression.id === 5 && "RM"}
                   <CountUp duration={1.5} end={progression.count} />
                 </div>
