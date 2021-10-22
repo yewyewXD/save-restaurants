ed846fedb0a076dcae27be60f974d74616816dd7
yewyewXD <yewyew6933@gmail.com>
2021-10-22 21:01:27 +0200

    finish layout of HowItWorks section

--- frontend/src/pages/Home/components/HowItWorksSection.tsx
+++ frontend/src/pages/Home/components/HowItWorksSection.tsx
@@ -2,13 +2,50 @@ import React from "react";
 
 const HowItWorksSection = () => {
   return (
-    <section
-      className="flex w-full justify-center items-center flex-col py-16"
-      style={{ minHeight: "90vh" }}
-    >
-      <h1 className="leading-none text-4xl font-bold mb-8">How it works</h1>
+    <section className="w-full py-16" style={{ minHeight: "90vh" }}>
+      <div className="container mx-auto">
+        <h1 className="leading-none text-4xl font-bold mb-20 text-center">
+          How it works
+        </h1>
 
-      <div>testing</div>
+        <div className="grid grid-cols-2 w-full mb-20">
+          <div className="flex items-center justify-start">testing 1</div>
+          <div className="flex">
+            <div className="flex justify-center items-center font-bold pr-12 text-3xl">
+              1
+            </div>
+
+            <div>
+              <div className="text-2xl mb-3 font-bold">Choose your product</div>
+              <div>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic
+                rerum, officiis mollitia earum, porro adipisci, veniam ea omnis
+                eum doloribus pariatur quas mole
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-2 w-full">
+          <div className="order-last flex items-center justify-end">
+            testing 1
+          </div>
+          <div className="flex">
+            <div className="flex justify-center items-center font-bold pr-12 text-3xl">
+              2
+            </div>
+
+            <div>
+              <div className="text-2xl mb-3 font-bold">Choose your product</div>
+              <div>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic
+                rerum, officiis mollitia earum, porro adipisci, veniam ea omnis
+                eum doloribus pariatur quas mole
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
