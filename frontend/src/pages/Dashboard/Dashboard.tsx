59e88257d54267fdb2a6defb88a7011790cf406d
yewyewXD <yewyew6933@gmail.com>
2021-10-25 12:20:49 +0200

    adjust logo into nav sidebar

--- frontend/src/pages/Dashboard/Dashboard.tsx
+++ frontend/src/pages/Dashboard/Dashboard.tsx
@@ -10,7 +10,7 @@ const Dashboard: FC = () => {
       <NavSidebar currentTab={currentTab} setCurrentTab={setCurrentTab} />
 
       <div className="flex-grow">
-        <div className="h-12"></div>
+        <div className="h-20"></div>
 
         <div className="py-4 px-8 w-full h-full">
           <SiteAll />
--- frontend/src/pages/Dashboard/components/NavSidebar.tsx
+++ frontend/src/pages/Dashboard/components/NavSidebar.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from "react";
 import styles from "./NavSidebar.module.scss";
+import { Link } from "react-router-dom";
 interface Props {
   currentTab: string;
   setCurrentTab: Function;
@@ -15,7 +16,14 @@ const NavSidebar: FC<Props> = ({ currentTab, setCurrentTab }) => {
     //  w-52 xl:w-64 2xl:w-80
     <div className="relative min-h-screen h-full w-64">
       <div className="h-screen fixed left-0 top-0 bg-black text-white w-64">
-        <div className="h-12"></div>
+        <div className="h-20 p-8 pb-0">
+          <Link
+            to="/"
+            className="font-bold text-4xl tracking-tight leading-none block text-left"
+          >
+            <span className="text-primary">e</span>atery
+          </Link>
+        </div>
 
         <div className="p-4">
           {sidebarTabs.map((tab, index) => (
