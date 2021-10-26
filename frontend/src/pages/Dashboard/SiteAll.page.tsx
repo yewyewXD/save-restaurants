926c00dc95977e50d2ec1500ada8843a9cb36839
yewyewXD <yewyew6933@gmail.com>
2021-10-26 08:33:32 +0200

    add searchbar beside create button

--- frontend/src/pages/Dashboard/SiteAll.page.tsx
+++ frontend/src/pages/Dashboard/SiteAll.page.tsx
@@ -28,13 +28,27 @@ const SiteAll = () => {
       <div className="flex justify-between items-center mb-8">
         <div className="leading-none text-3xl">All My Sites</div>
 
-        <Link
-          to="/dashboard/sites/new"
-          className="flex items-center justify-center bg-primary px-6 py-3 rounded hover:text-white hover:bg-black transition duration-200 font-bold text-black hover:border-black"
-        >
-          <i className="icon-inno icon-inno_plus mr-3" />
-          <span>Create New </span>
-        </Link>
+        <div className="flex">
+          <div className="bg-white shadow px-2 mr-3 flex">
+            <span className="w-auto flex justify-end items-center text-gray-500 p-2">
+              <i className="icon-inno icon-inno_search" />
+            </span>
+
+            <input
+              className="w-full rounded p-2 outline-none"
+              type="text"
+              placeholder="Search"
+            />
+          </div>
+
+          <Link
+            to="/dashboard/sites/new"
+            className="flex items-center justify-center bg-primary px-6 py-3 rounded hover:text-white hover:bg-black transition duration-200 font-bold text-black hover:border-black"
+          >
+            <i className="icon-inno icon-inno_plus mr-3" />
+            <span>Create New </span>
+          </Link>
+        </div>
       </div>
 
       <div className="grid grid-cols-3 gap-16 auto-rows-auto">
