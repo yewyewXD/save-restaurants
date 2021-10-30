2523465d5aff916175b1dbad1cf8b78d347d4bc0
yewyewXD <yewyew6933@gmail.com>
2021-10-30 16:57:29 +0200

    setup react color picker for cta button

--- frontend/src/templates/template-1/components/HeroSection.tsx
+++ frontend/src/templates/template-1/components/HeroSection.tsx
@@ -1,47 +1,75 @@
-import React, { FC, useCallback } from "react";
+import React, { FC, useState } from "react";
 import ImageUploader from "../../../globalUI/forms/ImageUploader";
 import HoverEffect from "../../../globalUI/Site/HoverEffect";
 import { ISectionProps } from "../template1.types";
+import { HexColorPicker } from "react-colorful";
+import OutsideClick from "react-outside-click-handler";
+
+const HeroEdit: FC = () => {
+  const [ctaColor, setCtaColor] = useState("#000000");
+  const [showColorPicker, setShowColorPicker] = useState(false);
+
+  return (
+    <>
+      <div className="mb-8">
+        <div className="mb-2 font-bold">Background</div>
+        <ImageUploader />
+      </div>
+
+      <div className="mb-8">
+        <div className="mb-2 font-bold">Title</div>
+        <input
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          type="text"
+          placeholder="Username"
+        />
+      </div>
+
+      <div className="mb-8">
+        <div className="mb-2 font-bold">Description</div>
+        <input
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          type="text"
+          placeholder="Username"
+        />
+      </div>
+
+      <div className="mb-8">
+        <div className="mb-2 font-bold flex justify-start items-center">
+          <span>Button</span>
+
+          <OutsideClick
+            onOutsideClick={() => {
+              if (showColorPicker) setShowColorPicker(false);
+            }}
+          >
+            <div
+              className="ml-2 w-4 h-4 rounded-full cursor-pointer relative"
+              style={{ backgroundColor: ctaColor }}
+              onClick={(): void => {
+                if (!showColorPicker) setShowColorPicker(true);
+              }}
+            >
+              {showColorPicker && (
+                <div className="absolute left-6 top-0">
+                  <HexColorPicker color={ctaColor} onChange={setCtaColor} />
+                </div>
+              )}
+            </div>
+          </OutsideClick>
+        </div>
+        <input
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          type="text"
+          placeholder="Username"
+          value="Button to menu"
+        />
+      </div>
+    </>
+  );
+};
 
 const HeroSection: FC<ISectionProps> = ({ handleOpenMenu }) => {
-  const HeroEdit = useCallback(() => {
-    return (
-      <>
-        <div className="mb-8">
-          <div className="mb-2 font-bold">Background</div>
-          <ImageUploader />
-        </div>
-
-        <div className="mb-8">
-          <div className="mb-2 font-bold">Title</div>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            type="text"
-            placeholder="Username"
-          />
-        </div>
-
-        <div className="mb-8">
-          <div className="mb-2 font-bold">Description</div>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            type="text"
-            placeholder="Username"
-          />
-        </div>
-
-        <div className="mb-8">
-          <div className="mb-2 font-bold">Button</div>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            type="text"
-            placeholder="Username"
-          />
-        </div>
-      </>
-    );
-  }, []);
-
   return (
     <section
       style={{
@@ -58,7 +86,9 @@ const HeroSection: FC<ISectionProps> = ({ handleOpenMenu }) => {
         elementName="Hero section"
       />
       <div className="container flex justify-center items-center flex-col">
-        <h1 className="text-5xl leading-none ">Main home section text</h1>
+        <h1 className="text-5xl leading-none text-center">
+          Main home section text
+        </h1>
         <span className="my-6 leading-none ">Subtitle of home section</span>
 
         <button className="bg-primary px-9 py-3 rounded text-black hover:text-white hover:bg-black transition duration-200 font-bold">
