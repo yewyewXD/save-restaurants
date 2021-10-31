4715533732f1225ad01c0b2f5c166fa75c5665de
yewyewXD <yewyew6933@gmail.com>
2021-10-31 12:22:22 +0100

    add addMenu feature in menuEdit

--- frontend/src/templates/template-1/components/MenuSection.tsx
+++ frontend/src/templates/template-1/components/MenuSection.tsx
@@ -2,22 +2,7 @@ import React, { useState } from "react";
 import HoverEffect from "../../../globalUI/Site/HoverEffect";
 import { ISectionProps } from "../template1.types";
 
-const MenuEdit: React.FC = () => (
-  <>
-    <h1 className="mb-6 text-2xl">Menu Section</h1>
-
-    <div className="mb-8">
-      <div className="mb-2 font-bold">Title</div>
-      <input
-        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-        type="text"
-        placeholder="Username"
-      />
-    </div>
-  </>
-);
-
-const MenuSection: React.FC<ISectionProps> = ({ handleOpenMenu }) => {
+const MenuEdit: React.FC = () => {
   const [menuCategories, setMenuCategories] = useState([
     {
       id: "menu-category-1",
@@ -51,48 +36,36 @@ const MenuSection: React.FC<ISectionProps> = ({ handleOpenMenu }) => {
     ]);
   }
 
-  function handleRemoveMenu(menuId: string) {
-    setMenuCategories((prevMenus) =>
-      prevMenus.filter((menu) => menu.id !== menuId)
-    );
-  }
-
-  function handleAddMenuItem(menuId: string) {
-    setMenuCategories((prevMenus) => {
-      return prevMenus.map((menu) => {
-        if (menu.id === menuId) {
-          return {
-            ...menu,
-            items: [
-              ...menu.items,
-              { name: `food ${menu.items.length + 1}`, price: "$ 1.00" },
-            ],
-          };
-        } else {
-          return menu;
-        }
-      });
-    });
-  }
-
   return (
-    <section
-      className="w-full flex justify-center items-center flex-col py-20 borderOnHover"
-      id="menu-section"
-    >
-      <HoverEffect
-        onClick={() => {
-          handleOpenMenu(<MenuEdit />);
-        }}
-        elementName="Menu Section"
-      />
-      <h1 className="text-4xl leading-none text-center mb-10">Menu</h1>
-      <div className="grid grid-cols-3 gap-10 px-10 auto-rows-auto w-full text-center">
+    <>
+      <h1 className="mb-6 text-2xl">Menu Section</h1>
+
+      <div className="mb-8">
+        <div className="mb-2 font-bold">Title</div>
+        <input
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          type="text"
+          placeholder="Username"
+        />
+      </div>
+
+      <div>
+        <div className="mb-2 flex items-center">
+          <span className="font-bold">Menu Categories</span>
+
+          <div
+            className="ml-2 w-6 h-6 rounded-full cursor-pointer flex justify-center items-center border border-black hover:bg-blue-600 hover:border-blue-600 hover:text-white transition duration-200"
+            onClick={handleAddMenu}
+          >
+            +
+          </div>
+        </div>
+
         {menuCategories.map((menu, index) => {
           return (
             <div
               key={menu.id}
-              className="relative border-gray-700 border rounded p-4"
+              className="relative border-gray-700 border rounded p-4 mb-6"
             >
               <div className="flex justify-center items-center">
                 <h5 className="mb-6 w-max">{menu.title}</h5>
@@ -114,7 +87,49 @@ const MenuSection: React.FC<ISectionProps> = ({ handleOpenMenu }) => {
             </div>
           );
         })}
+      </div>
+    </>
+  );
+};
 
+const MenuSection: React.FC<ISectionProps> = ({ handleOpenMenu }) => {
+  // function handleRemoveMenu(menuId: string) {
+  //   setMenuCategories((prevMenus) =>
+  //     prevMenus.filter((menu) => menu.id !== menuId)
+  //   );
+  // }
+
+  // function handleAddMenuItem(menuId: string) {
+  //   setMenuCategories((prevMenus) => {
+  //     return prevMenus.map((menu) => {
+  //       if (menu.id === menuId) {
+  //         return {
+  //           ...menu,
+  //           items: [
+  //             ...menu.items,
+  //             { name: `food ${menu.items.length + 1}`, price: "$ 1.00" },
+  //           ],
+  //         };
+  //       } else {
+  //         return menu;
+  //       }
+  //     });
+  //   });
+  // }
+
+  return (
+    <section
+      className="w-full flex justify-center items-center flex-col py-20 borderOnHover"
+      id="menu-section"
+    >
+      <HoverEffect
+        onClick={() => {
+          handleOpenMenu(<MenuEdit />);
+        }}
+        elementName="Menu Section"
+      />
+      <h1 className="text-4xl leading-none text-center mb-10">Menu</h1>
+      <div className="grid grid-cols-3 gap-10 px-10 auto-rows-auto w-full text-center">
         {/* <div className="flex items-center">
           <div
             className="flex justify-center items-center mt-3 border border-black rounded p-2 cursor-pointer h-36 w-full transition duration-200 hover:bg-gray-200"
