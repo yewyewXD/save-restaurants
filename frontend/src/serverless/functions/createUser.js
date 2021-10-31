aca609329b3c647adfe0f3f01b3e62dbdf1af69f
yewyewXD <yewyew6933@gmail.com>
2021-10-31 10:04:05 +0100

    setup create user serverless api & schema

--- frontend/src/serverless/functions/createUser.js
+++ frontend/src/serverless/functions/createUser.js
@@ -1,11 +1,29 @@
-'use strict';
+const AWS = require("aws-sdk");
 
 module.exports.createUser = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
+  const { username, email, password } = JSON.parse(event.body);
+  const newUserParams = {
+    TableName: process.env.DYNAMO_USER_TABLE,
+    Item: {
+      pk: username,
+      password,
+    },
   };
+
+  try {
+    const dynamoDb = new AWS.DynamoDB.DocumentClient();
+    const res = await dynamoDb.put(newUserParams).promise();
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+        "Access-Control-Allow-Headers": "Authorization",
+      },
+    };
+  } catch (err) {
+    console.log(err);
+    return new Error("There was an error creating a user");
+  }
 };
